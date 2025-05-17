d98b5b1fe177e6e4869e3bf33ec19b842470f9f0
dhruvimehta17 <dhruvimehta1702@gmail.com>
2025-05-17 17:31:40 +0530

    use dynamic db path for Render deployment

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./db.sqlite');
+const path = require('path');
+
+const dbPath = process.env.NODE_ENV === 'production'
+  ? '/var/data/db.sqlite'
+  : './db.sqlite';
+
+const db = new sqlite3.Database(dbPath);
 module.exports = db;
